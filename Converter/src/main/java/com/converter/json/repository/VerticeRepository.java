125a540c1af7e2670b99b22b52be4fabb515ecde
owen <wavefunctionc@gmail.com>
2023-08-25 10:12:35 +0900

    Add Vertice

---
+++ Converter/src/main/java/com/converter/json/repository/VerticeRepository.java
@@ -0,0 +1,7 @@
+package com.converter.json.repository;
+
+import com.converter.json.entity.Vertice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerticeRepository extends JpaRepository<Vertice, Long> {
+}
