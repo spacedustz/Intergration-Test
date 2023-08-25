7a6361f482e528ed9ef5e8c0f551bf673157df95
owen <wavefunctionc@gmail.com>
2023-08-25 17:16:09 +0900

    Add React - Functional Component & Props

--- React-TS/src/App.tsx
+++ React-TS/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import ReactiveVar from "./components/ReactiveFC";
 
 function App() {
     return (
         <div>
-
+            <ReactiveVar items={['A', 'B']} />
         </div>
     );
 }
