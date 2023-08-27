354e4ca7bda44baebaad2c4dcd6ea9b79685d4d1
owen <wavefunctionc@gmail.com>
2023-08-27 11:40:50 +0900

    React - Refs

--- React-TS/src/App.tsx
+++ React-TS/src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css'
-import ReactiveVar from "./components/ReactiveFC";
+import ReactiveFC from "./components/ReactiveFC";
 import Reactive from "./models/data";
+import RefInput from "./components/RefInput";
 
 function App() {
+    // ReactiveFC 컴포넌트를 위한 더미 데이터
+    // RefInput으로 폼 제출하면 여기에 추가 돠어야함
     const item = [
         new Reactive('A'),
         new Reactive('B')
@@ -10,7 +13,8 @@ function App() {
 
     return (
         <div>
-            <ReactiveVar items={item} />
+            <RefInput />
+            <ReactiveFC items={item} />
         </div>
     );
 }
