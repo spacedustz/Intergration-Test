4f748da3a152a6db659a69abd0227e0a63b8923d
Kunwoo Shin <recyclefunction@gmail.com>
2023-08-27 13:07:08 +0900

    React - 항목 삭제

--- React-TS/src/components/RefInput.tsx
+++ React-TS/src/components/RefInput.tsx
@@ -1,20 +1,23 @@
 import React, {useRef} from "react";
 
-const Input = () => {
+const Input: React.FC<{onAddItem: (enteredText: string) => void}> = (props) => {
+    // Input Ref
+    const inputRef = useRef<HTMLInputElement>(null);
+
     // Form 입력 시, Browser Default 방지
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault();
+
+        const enteredText = inputRef.current?.value;
+
+        // Input 검증
+        if (enteredText.trim().length === 0) {
+            // Throw an Error
+            return;
+        }
+
+        props.onAddItem(enteredText);
     };
-
-    // Input Ref
-    const inputRef = useRef<HTMLInputElement>(null);
-    const enteredText = inputRef.current?.value;
-
-    // Input 검증
-    if (enteredText.trim().length === 0) {
-        // Throw an Error
-        return;
-    }
 
     return <form onSubmit={submitHandler}>
         <label htmlFor="text">Text Here</label>
