d9ced727087e885442cdbd77e23b12cb7e4e0ca5
owen <wavefunctionc@gmail.com>
2023-08-28 17:08:58 +0900

    React - TimeGraph 컴포넌트 수정

--- React-TS/src/components/chart/TimeGraph.tsx
+++ React-TS/src/components/chart/TimeGraph.tsx
@@ -2,7 +2,13 @@
 // import { Line } from 'react-chartjs-2';
 // import axios from 'axios';
 // import * as _ from 'lodash';
-// import { Chart, ChartData, ChartOptions, registerables } from 'chart.js';
+// import {
+//     Chart,
+//     ChartData,
+//     ChartOptions,
+//     registerables,
+//     BubbleDataPoint,
+// } from 'chart.js';
 //
 // interface FrameData {
 //     count: number;
@@ -27,7 +33,10 @@
 //
 // const getMinutesFromSystemDate = (systemDate: number[]): string => {
 //     const [, , , , minute, second] = systemDate;
-//     return `${String(minute).padStart(2, '0')}:${String(second).padStart(2, '0')}`;
+//     return `${String(minute).padStart(2, '0')}:${String(second).padStart(
+//         2,
+//         '0',
+//     )}`;
 // };
 //
 // export const ChartComponent = () => {
@@ -50,22 +59,29 @@
 //
 //     if (!frameData.length) return null;
 //
-//     const groupedByKey = _.groupBy(frameData, frame => getMinutesFromSystemDate(frame.systemDate));
+//     const groupedByKey = _.groupBy(frameData, (frame) =>
+//         getMinutesFromSystemDate(frame.systemDate),
+//     );
 //
-//     const lineChartData: ChartData<'line'> = {
+//     const lineChartData: ChartData<'line', BubbleDataPoint[]> = {
+//         // 아래 코드 중 maxCountValues를 초기화 할 때 dataPoint의 타입을 BubbleDataPoint로 좁히기 위해 chart.js에서 BubbleDataPoint를 import하고 타입옵션에 두번째 BubbleDataPoint[]를 넣었습니다.
 //         datasets: [
 //             {
 //                 label: '보안 이벤트',
-//                 data: Object.keys(groupedByKey).map(timeKey => {
+//                 data: Object.keys(groupedByKey).map((timeKey) => {
 //                     const framesForThisTime = groupedByKey[timeKey];
-//                     const maxCountForThisTime = Math.max(...framesForThisTime.map(frame => frame.count));
+//                     const maxCountForThisTime = Math.max(
+//                         ...framesForThisTime.map((frame) => frame.count),
+//                     );
 //                     const firstFrameId = framesForThisTime[0].frameId;
-//                     const lastFrameId = framesForThisTime[framesForThisTime.length - 1].frameId;
+//                     const lastFrameId =
+//                         framesForThisTime[framesForThisTime.length - 1].frameId;
 //
 //                     return {
-//                         x: timeKey,
+//                         x: parseInt(timeKey), // x의 값을 number타입으로 변경
 //                         y: maxCountForThisTime,
-//                         frameId: `${firstFrameId}-${lastFrameId}`,
+//                         r: firstFrameId - lastFrameId, // r의 값을 number타입으로 변경
+//                         // BubbleDataPoint 타입의 key로 x,y,r만 올 수 있는 것 같아서 key를 변경(frameId -> r)했어요.
 //                     };
 //                 }),
 //                 backgroundColor: ['lightblue'],
@@ -77,7 +93,9 @@
 //         ],
 //     };
 //
-//     const maxCountValues = lineChartData.datasets[0].data.map(dataPoint => dataPoint.y);
+//     const maxCountValues = lineChartData.datasets[0].data.map(
+//         (dataPoint) => dataPoint.y,
+//     );
 //
 //     const lineChartOptions: ChartOptions<'line'> = {
 //         responsive: true,
@@ -93,8 +111,12 @@
 //                         const dataIndex: number = context.dataIndex;
 //                         const datasetIndex: number = context.datasetIndex;
 //
-//                         if (typeof dataIndex !== 'undefined' && typeof datasetIndex !== 'undefined') {
-//                             const frameIdRange = lineChartData.datasets[datasetIndex].data[dataIndex].frameId;
+//                         if (
+//                             typeof dataIndex !== 'undefined' &&
+//                             typeof datasetIndex !== 'undefined'
+//                         ) {
+//                             const frameIdRange =
+//                                 lineChartData.datasets[datasetIndex].data[dataIndex].r; // frameId -> r
 //                             if (frameIdRange) {
 //                                 return `프레임 ID: ${frameIdRange}`;
 //                             }
@@ -119,14 +141,15 @@
 //                 title: { display: true, text: '개수' },
 //                 min: Math.min(...maxCountValues),
 //                 max: Math.max(...maxCountValues),
-//                 ticks: { stepSize: 1, beginAtZero: true },
+//                 ticks: { stepSize: 1 },
+//                 beginAtZero: true, // thicks의 값인 객체 안에 들어있으면 정의되지 않은 key라고 나와서 chartjs의 타입들을 살펴보니 따로 적는것같아서 분리했어요.
 //             },
 //         },
 //     };
 //
 //     return (
 //         <div>
-//             <h2 align="center">라인 차트</h2>
+//             <h2>라인 차트</h2>
 //             <div style={{ overflow: 'auto', maxWidth: '1000px', maxHeight: '800px' }}>
 //                 <Line data={lineChartData} options={lineChartOptions} />
 //             </div>
