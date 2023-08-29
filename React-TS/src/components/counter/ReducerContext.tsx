4050de0ec78686e41d1d4a8ac82305ea44232c52
owen <wavefunctionc@gmail.com>
2023-08-29 14:38:30 +0900

    React - Update Reducer + Context API

--- React-TS/src/components/counter/ReducerContext.tsx
+++ React-TS/src/components/counter/ReducerContext.tsx
@@ -1,4 +1,4 @@
-import React, {useReducer} from "react";
+import React, {createContext, Dispatch, useContext, useReducer} from "react";
 
 // 필요한 타입 선언
 type Color = 'red' | 'orange' | 'yellow';
@@ -18,6 +18,13 @@ type Action =
     | { type: 'SET_COLOR'; color: Color }
     | { type: 'TOGGLE_GOOD' };
 
+// 디스패치를 위한 타입, (Dispatch 를 리액트에서 불러올 수 있음), 액션들의 타입을 Dispatch 의 Generics로 설정
+type SampleDispatch = Dispatch<Action>;
+
+// Context 생성
+const StateContext = createContext<State | null>(null);
+const DispatchContext = createContext<SampleDispatch | null>(null);
+
 // Reducer 함수
 function reducer(state: State, action: Action): State {
     switch (action.type) {
@@ -29,7 +36,7 @@ function reducer(state: State, action: Action): State {
     }
 }
 
-export default function CounterReducer() {
+export default function ReducerContext({children}: {children: React.ReactNode}) {
 
     // 초기 State 값
     const initState: State = {
@@ -42,26 +49,24 @@ export default function CounterReducer() {
     // Use Reducer 1. Reducer함수, 초기 상태값
     const [state, dispatch] = useReducer(reducer, initState);
 
-    // DisPatch를 이용한 상태 변경
-    const setCount = () => dispatch({ type: 'SET_COUNT', count: 5 }); // count 를 넣지 않으면 에러발생
-    const setText = () => dispatch({ type: 'SET_TEXT', text: 'bye' }); // text 를 넣지 않으면 에러 발생
-    const setColor = () => dispatch({ type: 'SET_COLOR', color: 'orange' }); // orange 를 넣지 않으면 에러 발생
-    const toggleGood = () => dispatch({ type: 'TOGGLE_GOOD' });
-
-    // 출력
     return (
-        <div>
-            <p><code>count: </code> {state.count}</p>
-            <p><code>text: </code> {state.text}</p>
-            <p><code>color: </code> {state.color}</p>
-            <p><code>isGood: </code> {state.isGood ? 'true' : 'false'}</p>
-
-            <div>
-                <button onClick={setCount}>SET_COUNT</button>
-                <button onClick={setText}>SET_TEXT</button>
-                <button onClick={setColor}>SET_COLOR</button>
-                <button onClick={toggleGood}>TOGGLE_GOOD</button>
-            </div>
-        </div>
+        <StateContext.Provider value={state}>
+            <DispatchContext.Provider value={dispatch}>
+                {children}
+            </DispatchContext.Provider>
+        </StateContext.Provider>
     );
+}
+
+// State와 Dispatch를 쉽게 사용하기 위한 Custom Hook
+export function useCounterState() {
+    const state = useContext(StateContext);
+    if (!state) throw new Error('Cannot find Provider');
+    return state;
+}
+
+export function useCounterDispatch() {
+    const dispatch = useContext(DispatchContext);
+    if (!dispatch) throw new Error('Cannot find Provider');
+    return dispatch;
 }
