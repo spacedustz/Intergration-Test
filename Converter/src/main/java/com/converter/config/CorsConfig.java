5f9f2535a1c21d6e4efd8c7f2dbfaf2ff0cf719f
Kunwoo Shin <recyclefunction@gmail.com>
2023-09-07 23:16:18 +0900

    Backend Socket 설정 변경

--- Converter/src/main/java/com/converter/config/CorsConfig.java
+++ Converter/src/main/java/com/converter/config/CorsConfig.java
@@ -26,7 +26,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/csv")
+        registry.addMapping("*")
                 .allowedOrigins("http://localhost:3000") // 허용할 origin (프론트엔드 주소)
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 허용할 헤더
