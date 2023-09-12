51590f6d8eb9f8ccc68b0ca469bd17d573f5ea70
owen <wavefunctionc@gmail.com>
2023-09-12 14:02:18 +0900

    WebSocket Config Origin Pattrn 수정

--- Converter/src/main/java/com/converter/config/WebSocketConfig.java
+++ Converter/src/main/java/com/converter/config/WebSocketConfig.java
@@ -15,11 +15,9 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final StompFrameHandler handler;
-
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        // 이 토픽을 구독하면 Subscriber들에게 메시지를 브로드캐스팅 함
+        // 이 토픽을 구독하면 Subscriber 들에게 메시지를 브로드캐스팅 함
         config.enableStompBrokerRelay("/topic");
 
         // 메시지 발행 요청할 때 사용
@@ -29,8 +27,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // WebSocket 연결 엔드포인트 설정, ex) ws://localhost:18080/ws
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
-                .withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 }
