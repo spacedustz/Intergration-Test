c9c6b47eaf5a21e239a16b2d60ac58066159258b
owen <wavefunctionc@gmail.com>
2023-09-13 15:14:59 +0900

    Redis Controller 수정 완료

---
+++ Converter/src/main/java/com/converter/config/interceptor/HttpHandshakeInterceptor.java
@@ -0,0 +1,32 @@
+package com.converter.config.interceptor;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+import static org.springframework.messaging.simp.stomp.StompHeaders.SESSION;
+
+public class HttpHandshakeInterceptor implements HandshakeInterceptor {
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request,
+                                   ServerHttpResponse response,
+                                   WebSocketHandler wsHandler,
+                                   Map<String, Object> attributes) throws Exception {
+        if (request instanceof ServletServerHttpRequest servletRequest) {
+            HttpSession session = servletRequest.getServletRequest().getSession();
+            attributes.put(SESSION, session);
+        }
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request,
+                               ServerHttpResponse response,
+                               WebSocketHandler wsHandler,
+                               Exception exception) {}
+}
